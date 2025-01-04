e260af8b11bcd77c95bc3941bb97a4edc3e28609
devance <dj788@snu.edu.in>
2025-01-04 14:13:57 +0530

    some updates

---
+++ types/generated/components.d.ts
@@ -0,0 +1,74 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'Seo';
+    icon: 'slideshow';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text;
+    metaTitle: Schema.Attribute.String;
+    ogImage: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface SharedText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_texts';
+  info: {
+    description: '';
+    displayName: 'Text';
+    icon: 'pencil';
+  };
+  attributes: {
+    content: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+export interface UiComponentsBlockquote extends Struct.ComponentSchema {
+  collectionName: 'components_ui_components_blockquotes';
+  info: {
+    displayName: 'blockquote';
+    icon: 'underline';
+  };
+  attributes: {
+    content: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface UiComponentsPdf extends Struct.ComponentSchema {
+  collectionName: 'components_ui_components_pdfs';
+  info: {
+    displayName: 'PDF';
+    icon: 'filePdf';
+  };
+  attributes: {
+    pdf: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface UiComponentsVideo extends Struct.ComponentSchema {
+  collectionName: 'components_ui_components_videos';
+  info: {
+    displayName: 'Video';
+    icon: 'television';
+  };
+  attributes: {
+    url: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.seo': SharedSeo;
+      'shared.text': SharedText;
+      'ui-components.blockquote': UiComponentsBlockquote;
+      'ui-components.pdf': UiComponentsPdf;
+      'ui-components.video': UiComponentsVideo;
+    }
+  }
+}
